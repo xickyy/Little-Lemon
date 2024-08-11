4e7663ee3ab67b5fda30203961d3e4c69ad04510
xickyy <ricky_moore@ymail.com>
2024-08-11 16:57:01 -0700

    figured out the difference between a composable and a views empty starter project

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -20,35 +20,35 @@ class MainActivity : ComponentActivity() {
     lateinit var toplayout : LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        toplayout=findViewById(R.id.toplayout)
-        toplayout.setBackgroundColor(Color.parseColor("#495E57"))
-        var textview = TextView(this)
-        textview.text=resources.getText(R.string.little_lemon)
-        textview.setTextColor(Color.parseColor("#F4CE14"))
-        toplayout.addView(textview)
+//        setContentView(R.layout.activity_main)
+//        toplayout=findViewById(R.id.toplayout)
+//        toplayout.setBackgroundColor(Color.parseColor("#495E57"))
+//        var textview = TextView(this)
+//        textview.text=resources.getText(R.string.little_lemon)
+//        textview.setTextColor(Color.parseColor("#F4CE14"))
+//        toplayout.addView(textview)
 
-//        setContent {
-//            LittleLemonTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
+        setContent {
+            LittleLemonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainPanel("Little Lemon")
+                }
+            }
+        }
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello Ricky!",
-//        modifier = modifier
-//    )
-//}
+@Composable
+fun MainPanel(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 //
 //@Preview(showBackground = true)
 //@Composable
