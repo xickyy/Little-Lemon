93970f24fc37850ebf85a1c012d57e0b40c8104d
xickyy <ricky_moore@ymail.com>
2024-08-10 22:55:13 -0700

    dynamically linked my view through kotlin code

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.littlelemon
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,34 +16,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var toplayout : LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LittleLemonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        toplayout=findViewById(R.id.toplayout)
+        toplayout.setBackgroundColor(Color.parseColor("#495E57"))
+        var textview = TextView(this)
+        textview.text=resources.getText(R.string.little_lemon)
+        textview.setTextColor(Color.parseColor("#F4CE14"))
+        toplayout.addView(textview)
+
+//        setContent {
+//            LittleLemonTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Ricky!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonTheme {
-        Greeting("Ricky")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello Ricky!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    LittleLemonTheme {
+//        Greeting("Ricky")
+//    }
+//}
