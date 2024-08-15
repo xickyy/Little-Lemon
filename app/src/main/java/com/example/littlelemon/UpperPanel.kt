5f9462777c9cf57a737bc39effd83bca562ca61a
xickyy <ricky_moore@ymail.com>
2024-08-14 21:08:43 -0700

    added functionality to the order button

--- app/src/main/java/com/example/littlelemon/UpperPanel.kt
+++ app/src/main/java/com/example/littlelemon/UpperPanel.kt
@@ -1,5 +1,6 @@
 package com.example.littlelemon
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -18,8 +19,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,6 +31,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun UpperPanel() {
+    val context = LocalContext.current
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
@@ -57,29 +61,37 @@ fun UpperPanel() {
         ) {
             Text(
                 text = stringResource(id = R.string.descriptionone),
-                modifier = Modifier.width(200.dp),
-                color = Color.White,
-                fontSize = 21.sp
+                color = Color(0xFFEDEFEE),
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(bottom = 28.dp)
+                    .fillMaxWidth(0.6f)
             )
             Image(
                 painter = painterResource(id = R.drawable.food),
-                contentDescription = "",
-                Modifier
-                    .height(200.dp)
+                contentDescription = "Upper Panel Image",
+                modifier = Modifier
                     .clip(RoundedCornerShape(20.dp))
+                    .height(150.dp)
+
 
             )
         }
 
 
         Button(
-            onClick = {},
-            shape = RoundedCornerShape(10.dp),
+            onClick = {
+                      Toast.makeText(context, "Order Successful!", Toast.LENGTH_SHORT).show()
+            },
+            shape = RoundedCornerShape(20.dp),
             modifier = Modifier.padding(horizontal = 20.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color(0XFFF4CE14))
         ) {
             Text(
-                text = stringResource(id = R.string.order)
+                text = stringResource(id = R.string.order),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF333333)
             )
         }
 
