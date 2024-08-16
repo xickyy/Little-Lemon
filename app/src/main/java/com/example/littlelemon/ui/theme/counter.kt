79e4d35249677d2a1cac6ebff530c3d37b569158
xickyy <ricky_moore@ymail.com>
2024-08-15 21:53:33 -0700

    creating a counter composabel

---
+++ app/src/main/java/com/example/littlelemon/ui/theme/counter.kt
@@ -0,0 +1,38 @@
+package com.example.littlelemon.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Counter() {
+    var count by remember {
+        mutableIntStateOf(0)
+    }
+    Card(elevation = 32.dp) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Greek Salad",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp
+            )
+        }
+    }
+}
